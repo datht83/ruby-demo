7f80c73db56224ae96a550042a86972e0cddbd56
Huynh Trung Dat <datht@cpu-datht.(none)>
2015-06-16 14:29:03 +0700

    add float

--- floating_point.rb
+++ floating_point.rb
@@ -1,7 +1,9 @@
 print "please enter the total amount of your bill: "
 bill_amount = gets.to_f
 print "What percentage tip do you want? "
-tip = gets.to_f
+tip = gets.to_f / 100
 
-puts bill_amount
-puts tip
+puts "your tip will be : #{bill_amount * tip}"
+#puts "your tip will be : " + (bill_amount * tip).to_s
+tip_amount = sprintf("$%.2f",bill_amount * tip)
+puts "your tip will be : #{tip_amount}"
