59601cf88818b5100c28416395ab2220eafaa204
datht83 <evolabledata@gmail.com>
2015-06-16 03:40:20 +0000

    update number

--- number.rb
+++ number.rb
@@ -3,4 +3,5 @@ age = gets.to_i
 puts "input your name"
 name = gets.chomp
 age_second = age * 365 * 24 * 60 * 60
+puts name + " is " + age_second.to_s + " second old."
 puts "#{name} is #{age_second} second old"
